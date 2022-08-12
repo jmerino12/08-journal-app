dc0fb519bd8cb8069c6668927a904bcf5d0cfc80
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-12 14:21:18 -0500

    Boton Flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
 import { ImageGallery } from "../components/ImageGallery";
 import { JornalLayout } from "../layout/JornalLayout";
 import { NothingSelectedView, NoteView } from "../view";
@@ -8,8 +9,22 @@ export const JournalPage = () => {
     <JornalLayout>
       {/*<Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsam dolorum esse incidunt cum cumque architecto pariatur consectetur nemo natus voluptatem voluptatibus magni, aperiam necessitatibus laborum odit reprehenderit ad fugit?</Typography>*/}
 
-      {/*<NothingSelectedView />*/}
-      <NoteView />
+      <NothingSelectedView />
+      {/*<NoteView />*/}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize3: 30 }} />
+      </IconButton>
     </JornalLayout>
   );
 };
