8f51472dce55eead4d9fc7f050bcbeae5d27d169
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-12 11:01:37 -0500

    NothingSelectedView - No hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,13 @@
-import { Typography } from '@mui/material';
-import { JornalLayout } from '../layout/JornalLayout';
-
+import { Typography } from "@mui/material";
+import { JornalLayout } from "../layout/JornalLayout";
+import { NothingSelectedView } from "../view";
 
 export const JournalPage = () => {
   return (
     <JornalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsam dolorum esse incidunt cum cumque architecto pariatur consectetur nemo natus voluptatem voluptatibus magni, aperiam necessitatibus laborum odit reprehenderit ad fugit?</Typography>
+      {/*<Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsam dolorum esse incidunt cum cumque architecto pariatur consectetur nemo natus voluptatem voluptatibus magni, aperiam necessitatibus laborum odit reprehenderit ad fugit?</Typography>*/}
 
+      <NothingSelectedView />
     </JornalLayout>
-  )
-}
+  );
+};
