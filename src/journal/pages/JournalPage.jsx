7fe11efcbb4b8cd45c040af4ba2ada6146a52e0a
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-12 14:15:22 -0500

    ImageList

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,15 @@
 import { Typography } from "@mui/material";
+import { ImageGallery } from "../components/ImageGallery";
 import { JornalLayout } from "../layout/JornalLayout";
-import { NothingSelectedView } from "../view";
+import { NothingSelectedView, NoteView } from "../view";
 
 export const JournalPage = () => {
   return (
     <JornalLayout>
       {/*<Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ipsam dolorum esse incidunt cum cumque architecto pariatur consectetur nemo natus voluptatem voluptatibus magni, aperiam necessitatibus laborum odit reprehenderit ad fugit?</Typography>*/}
 
-      <NothingSelectedView />
+      {/*<NothingSelectedView />*/}
+      <NoteView />
     </JornalLayout>
   );
 };
