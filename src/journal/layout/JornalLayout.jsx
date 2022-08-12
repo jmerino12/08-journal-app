0bdf39bf4c1597a8a670b91f89ceabb1be7dde9e
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-12 09:10:39 -0500

    NavBar

--- src/journal/layout/JornalLayout.jsx
+++ src/journal/layout/JornalLayout.jsx
@@ -1,18 +1,15 @@
-import { Box } from "@mui/system"
+import { Box } from "@mui/system";
+import { NavBar } from "../components";
 
 const drawerWidth = 240;
 
-
 export const JornalLayout = ({ children }) => {
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <Box component='main'
-                sx={{ flexGrow: 1, p: 3 }}>
-
-                {children}
-            </Box>
-
-
-        </Box>
-    )
-}
+  return (
+    <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {children}
+      </Box>
+    </Box>
+  );
+};
