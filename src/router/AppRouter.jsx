15875781bca694999a8ffe120784b078ccb92193
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-16 13:51:52 -0500

    Checking authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { AuthRoutes } from '../auth/routes/AuthRoutes'
-import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
-        <Route path='auth/*' element={<AuthRoutes />} />
-        <Route path='/*' element={<JournalRoutes />}/>
+      <Route path="auth/*" element={<AuthRoutes />} />
+      <Route path="/*" element={<JournalRoutes />} />
     </Routes>
-  )
-}
+  );
+};
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    status: "not-authenticated", //'not-auth', 'authenticated', 'checking'
+    status: "checking", //'not-auth', 'authenticated', 'checking'
     uid: null,
     email: null,
     displayName: null,
