020c9998d85197cb9a0c0332713dec5c9cf7e1d9
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-15 20:44:41 -0500

    Manejo de errores

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -10,8 +10,27 @@ const formData = {
   displayName: "",
 };
 
+const formValidation = {
+  email: [(value) => value.includes("@"), "El correo debe de tener un @"],
+  password: [
+    (value) => value.length >= 6,
+    "El password debe de tener mas de 6 caracteres.",
+  ],
+  displayName: [(value) => value.length >= 1, "El nombre es obligatorio."],
+};
+
 export const RegisterPage = () => {
-  const { displayName, email, password, onInputChange } = useForm(formData);
+  const {
+    formState,
+    displayName,
+    email,
+    password,
+    onInputChange,
+    isFormValid,
+    displayNameValid,
+    emailValid,
+    passwordValid,
+  } = useForm(formData, formValidations);
   const onSubmit = (event) => {};
   return (
     <AuthLayout title="Crear Cuenta">
