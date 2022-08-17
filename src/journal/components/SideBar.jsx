ea2d0cd543a700691eaad3020e8de0cdd4037e77
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-17 09:56:34 -0500

    Mostrar notas en el menu lateral

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -13,9 +13,11 @@ import {
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { useSelector } from "react-redux";
+import { SideBarItem } from "./SideBarItem";
 
 export const SideBar = ({ drawerWidth = 240 }) => {
   const { displayName } = useSelector((state) => state.auth);
+  const { notes } = useSelector((state) => state.journal);
   return (
     <Box
       component="nav"
@@ -36,20 +38,8 @@ export const SideBar = ({ drawerWidth = 240 }) => {
         </Toolbar>
         <Divider />
         <List>
-          {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TurnedInNot />
-                </ListItemIcon>
-                <Grid container>
-                  <ListItemText primary={text} />
-                  <ListItemText
-                    secondary={"Aute laborum cupidatat mollit nisi."}
-                  />
-                </Grid>
-              </ListItemButton>
-            </ListItem>
+          {notes.map((note) => (
+            <SideBarItem key={note.id} {...note} />
           ))}
         </List>
       </Drawer>
