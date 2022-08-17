87e43b1f1b7bcd7dfb38817bca13f585f0ff1739
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-08-17 10:05:17 -0500

    Activar una nota

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -7,13 +7,27 @@ import {
   ListItemText,
 } from "@mui/material";
 import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal";
 
-export const SideBarItem = ({ title, body, id }) => {
+export const SideBarItem = ({
+  title = "",
+  body,
+  id,
+  date,
+  imagesUrls = [],
+}) => {
+  const dispatch = useDispatch();
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
   }, [title]);
   return (
-    <ListItem disablePadding>
+    <ListItem
+      disablePadding
+      onClick={() =>
+        dispatch(setActiveNote({ title, body, id, date, imagesUrls }))
+      }
+    >
       <ListItemButton>
         <ListItemIcon>
           <TurnedInNot />
